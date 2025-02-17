90b01a4e73cf98680ba7b1f837b3c9f24e48f02b
Rodrigo Araujo <rasch.dev@gmail.com>
2025-02-17 15:53:28 -0300

    Created SellerJDBC and FactoryDAO

---
+++ src/model/dao/impl/SellerJDBC.java
@@ -0,0 +1,35 @@
+package model.dao.impl;
+
+import java.util.List;
+import model.dao.SellerDAO;
+import model.entities.Seller;
+
+public class SellerJDBC implements SellerDAO {
+
+	@Override
+	public void insert(Seller seller) {
+		
+	}
+
+	@Override
+	public void update(Seller seller) {
+		
+	}
+
+	@Override
+	public void deleteByID(Integer id) {
+		
+	}
+
+	@Override
+	public Seller findByID(Integer id) {
+		
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		
+		return null;
+	}
+}
